7350787d538b729b0e428f7aca46573e8cf9c4f7
k-yamamt <k-yamamt@ist.osaka-u.ac.jp>
2020-02-28 22:34:32 +0900

    'mktex2'

--- analysis/plot/plot.r
+++ analysis/plot/plot.r
@@ -1,11 +1,12 @@
-for (filename in list.files('C:/master/mstudy/data/news.yahoo.co.jp/', pattern = "-ping.csv", full.names = TRUE)){
+library('stringr')
+for (filename in list.files('C:/master/mstudy/data/aws.amazon.com/', pattern = '-ping.csv', full.names = TRUE)){
   df <- read.csv(file = filename, header = TRUE, sep=',')
   
   pdf(paste(str_sub(filename,-24,-9),'plot.pdf',sep=''))
   if (max(df$ping) > 135){
-    plot(df$ping, type="l", col="blue", xlab = 't', ylab = 'Delay[ms]',ylim = c(0,max(df$ping)+10))
+    plot(df$ping, type='l', col='blue', xlab = 't', ylab = 'Delay[ms]',ylim = c(0,max(df$ping)+10))
   }else{
-    plot(df$ping, type="l", col="blue", xlab = 't', ylab = 'Delay[ms]',ylim = c(0,140))
+    plot(df$ping, type='l', col='blue', xlab = 't', ylab = 'Delay[ms]',ylim = c(0,140))
   }
   dev.off()
 }

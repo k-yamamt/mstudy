59acb3908bad3ece2274b9aaa6885488d062a471
k-yamamt <k-yamamt@ist.osaka-u.ac.jp>
2020-02-28 20:53:34 +0900

    '2020/02/28-plot'

---
+++ analysis/plot/plot.r
@@ -0,0 +1,11 @@
+for (filename in list.files('C:/master/mstudy/data/news.yahoo.co.jp/', pattern = "-ping.csv", full.names = TRUE)){
+  df <- read.csv(file = filename, header = TRUE, sep=',')
+  
+  pdf(paste(str_sub(filename,-24,-9),'plot.pdf',sep=''))
+  if (max(df$ping) > 135){
+    plot(df$ping, type="l", col="blue", xlab = 't', ylab = 'Delay[ms]',ylim = c(0,max(df$ping)+10))
+  }else{
+    plot(df$ping, type="l", col="blue", xlab = 't', ylab = 'Delay[ms]',ylim = c(0,140))
+  }
+  dev.off()
+}

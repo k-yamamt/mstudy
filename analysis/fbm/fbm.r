965f57bc9f26f50823f2d65a04ef93bb03421fb4
k-yamamt <k-yamamt@ist.osaka-u.ac.jp>
2020-03-03 22:32:34 +0900

    'data'

--- analysis/fbm/fbm.r
+++ analysis/fbm/fbm.r
@@ -1,9 +1,10 @@
-library('bigmemory')
+library('SuperGauss')
 
-for (filename in list.files('C:/master/mstudy/data/aws.amazon.com/', pattern = '-ping.csv', full.names = TRUE)){
+addr = 'C:/master/mstudy/data/aws.amazon.com/'
+
+for (filename in list.files(addr, pattern = '-ping.csv', full.names = TRUE)){
   df <- read.csv(file = filename, header = TRUE, sep=',')
-  fbm_result <- as_FBM(df$ping)
-  
+
 }
-FBM()
+
 

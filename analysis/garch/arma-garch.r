7e436588f17fe1d8073b936f1cf10de21f8ceb6b
k-yamamt <k-yamamt@ist.osaka-u.ac.jp>
2020-03-22 21:12:47 +0900

    'barplot'

--- analysis/garch/arma-garch.r
+++ analysis/garch/arma-garch.r
@@ -9,7 +9,7 @@ identifyNumParam <- function(files){
   colnames(AIC) <- c('(p,q,r,s)','AIC')
   date <- NULL
   
-  for (filename in files){
+  for (filename in files[1:10]){
     df <- read.csv(file = filename, header = TRUE, sep=',')
     
     date <- c(date,str_sub(filename, start = -20, end = -14))
@@ -458,18 +458,28 @@ identifyNumParam <- function(files){
   AIC
 }
 
-regression <- function(files){
+regression <- function(files,id){
   for (filename in files){
     df <- read.csv(file = filename, header = TRUE, sep=',')
     
-    garch_result <- garchFit(formula = ~arma(2,2)+garch(1,1), data = df$ping, include.mean = TRUE, trace = FALSE)
-    y <- df$ping
-    predict.y <- garch_result@fitted
-    predict.y.upper <- y + 1.96*garch_result@h.t
-    predict.y.lower <- y - 1.96*garch_result@h.t
-    ymax <- 500
+    if(id == 'norm'){
+      garch_result <- garchFit(formula = ~arma(2,2)+garch(1,1), data = df$ping, include.mean = TRUE, trace = FALSE)
+      y <- df$ping
+      predict.y <- garch_result@fitted
+      predict.y.upper <- predict.y + 1.96*garch_result@sigma.t
+      predict.y.lower <- predict.y - 1.96*garch_result@sigma.t
+      pdf(paste(str_sub(filename,-20,-14),'-plot.pdf',sep=''))
+    }
+    else if(id == 'diff'){
+      garch_result <- garchFit(formula = ~arma(2,2)+garch(1,1), data = diff(df$ping), include.mean = TRUE, trace = FALSE)
+      y <- df$ping
+      predict.y <- c(y[1],y[1:length(y)-1]+garch_result@fitted)
+      predict.y.upper <- predict.y + c(0,1.96*garch_result@sigma.t)
+      predict.y.lower <- predict.y - c(0,1.96*garch_result@sigma.t)
+      pdf(paste(str_sub(filename,-20,-14),'-plot-diff.pdf',sep=''))
+    }
     
-    pdf(paste(str_sub(filename,-20,-14),'-plot.pdf',sep=''))
+    ymax <- 500
     plot(predict.y.upper, type='l', col='darkorange', ann = FALSE, axes = FALSE,xlim = c(0,length(predict.y.upper)+1),ylim = c(0,ymax), xaxs = "i", yaxs = "i")
     par(new = TRUE)
     plot(predict.y.lower, type='l', col='darkorange', ann = FALSE, axes = FALSE, xlim = c(0,length(predict.y.lower)+1),ylim = c(0,ymax), xaxs = "i", yaxs = "i")
@@ -490,30 +500,142 @@ regression <- function(files){
   }
 }
 
-params <- function(files){
+params <- function(files,id){
   D <- matrix(nrow = 0,ncol = 8)
-  colnames(AIC) <- c('c','a1','a2','b1','b2','omega','alpha1','beta1')
+  colnames(D) <- c('c','a1','a2','b1','b2','omega','alpha1','beta1')
   date <- NULL
   
   for (filename in files){
-    date <- c(date,str_sub(filename, start = -20, end = -14))
     df <- read.csv(file = filename, header = TRUE, sep=',')
-    garch_param <- garchFit(formula = ~arma(2,2)+garch(1,1), data = df$ping, include.mean = TRUE, trace = FALSE)@fit$coef
-    D <- rbind(D,c(garch_param[1],garch_param[2],garch_param[3],garch_param[4],garch_param[5],garch_param[6],garch_param[7],garch_param[8]))
+    
+    if(id == 'norm'){
+      tryCatch(
+        {
+          garch_param <- garchFit(formula = ~arma(2,2)+garch(1,1), data = df$ping, trace = FALSE)@fit$coef
+          date <- c(date,str_sub(filename, start = -20, end = -14))
+          D <- rbind(D,c(garch_param[1],garch_param[2],garch_param[3],garch_param[4],garch_param[5],garch_param[6],garch_param[7],garch_param[8]))
+        }
+        ,error = function(e){
+          cat(filename)
+          }
+        )
+    }else if(id == 'diff'){
+      tryCatch(
+        {
+          garch_param <- garchFit(formula = ~arma(2,2)+garch(1,1), data = diff(df$ping), trace = FALSE)@fit$coef
+          date <- c(date,str_sub(filename, start = -20, end = -14))
+          D <- rbind(D,c(garch_param[1],garch_param[2],garch_param[3],garch_param[4],garch_param[5],garch_param[6],garch_param[7],garch_param[8]))
+        }
+        ,error = function(e){
+          cat(filename)
+          }
+        )
+    }
   }
   
   rownames(D) <- date
   D
 }
 
-clustering <- function(){
+clustering <- function(id,dist.method,clustering.method,k){
+  if(id == 'norm'){
+    D <- Dnorm
+  }
+  else if(id == 'diff'){
+    D <- Ddiff
+  }
   
+  cluster <- cutree(hclust(dist(D,method = dist.method),method = clustering.method),k = k)
+  
+  # 時間帯
+  data <- matrix(0,nrow = 5, ncol = k)
+  rownames(data) <- c('03','07','12','17','20')
+  colnames(data) <- 1:k
+  for (i in 1:length(cluster)){
+    data[str_sub(names(cluster[i]),-2),cluster[i]] <- data[str_sub(names(cluster[i]),-2),cluster[i]] + 1
+  }
+  num <- colSums(data)
+  for (i in 1:length(num)){
+    data[,i] <- data[,i]/num[i]
+  }
+  par(mar=c(4,4,4,7))
+  bar <- barplot(
+    data,
+    names.arg = 1:k,
+    col = c('purple','green','red','darkorange','blue'),
+    xlab = 'クラスタ番号',
+    ylab = '割合'
+  )
+  par(xpd=T)
+  legend(par()$usr[2] + 0.2, par()$usr[4], legend = c('20:00-21:00','17:00-18:00','12:00-13:00','7:00-8:00','3:00-4:00'),
+         bty = "n", pch = 15, col = c('blue','darkorange','red','green','purple'))
+  
+  for (i in 1:length(num)){
+    text(num[i],x = bar[i], y = 1.05)
+  }
+  
+  #曜日
+  data <- matrix(0,nrow = 7, ncol = k)
+  rownames(data) <- c('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+  colnames(data) <- 1:k
+  for (i in 1:length(cluster)){
+    day <- str_sub(names(cluster[i]),start = 3, end = 4)
+    if(day == '29'){
+      id <- 'Sat'
+    }
+    else{
+      x <- as.integer(day)%%7
+      if(x == 1){
+        id <- 'Sun'
+      }
+      else if(x == 2){
+        id <- 'Mon'
+      }
+      else if(x == 3){
+        id <- 'Tue'
+      }
+      else if(x == 4){
+        id <- 'Wed'
+      }
+      else if(x == 5){
+        id <- 'Thu'
+      }
+      else if(x == 6){
+        id <- 'Fri'
+      }
+      else if(x == 0){
+        id <- 'Sat'
+      }
+    }
+    data[id,cluster[i]] <- data[id,cluster[i]] + 1
+  }
+  num <- colSums(data)
+  for (i in 1:length(num)){
+    data[,i] <- data[,i]/num[i]
+  }
+  par(mar=c(4,4,4,7))
+  bar <- barplot(
+    data,
+    names.arg = 1:k,
+    col = c('gray','firebrick3','darkturquoise','green','goldenrod3','blue','red'),
+    xlab = 'クラスタ番号',
+    ylab = '割合'
+  )
+  par(xpd=T)
+  legend(par()$usr[2] + 0.2, par()$usr[4], legend = c('日','土','金','木','水','火','月'),
+         bty = "n", pch = 15, col = c('red','blue','goldenrod3','green','darkturquoise','firebrick3','gray'))
+  
+  for (i in 1:length(num)){
+    text(num[i],x = bar[i], y = 1.05)
+  }
 }
 
-AIC <- identifyNumParam(files)
+#AIC <- identifyNumParam(files)
 
-#regression(files)
+#regression(files,'norm')
+#regression(files,'diff')
 
-#D <- params(files)
+#Dnorm <- params(files,'norm')
+#Ddiff <- params(files,'diff')
 
-#clustering()
+clustering('norm','euclidean','ward.D2',15)
